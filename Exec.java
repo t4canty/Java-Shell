6bc851c27a941df3d66f31e788e4442517450ab8
Screencap <t4canty@users.noreply.github.com>
2019-10-08 23:28:15 -0700

    Create Exec.java

---
+++ Exec.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Exec {
+	public static void rshell() {
+		System.out.println("Not implemented");
+	}
+	public static void exec(String cmd) {
+		System.out.println("Not implemented");
+	}
+}
