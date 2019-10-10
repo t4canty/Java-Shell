6fcc20c10ec75cf4bb975dceb749fb51d2ac7d7b
Screencap <t4canty@users.noreply.github.com>
2019-10-10 11:25:16 -0700

    Update Exec.java

--- Exec.java
+++ Exec.java
@@ -1,10 +1,34 @@
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Exec {
 	public static void rshell() {
 		System.out.println("Not implemented");
 	}
-	public static void exec(String cmd) {
-		System.out.println("Not implemented");
+	public static void exec(String[] cmd) {
+		String s;
+
+		try {
+			String cmdString = Arrays.toString(cmd);
+			cmdString = cmdString.replace(",", "");
+			cmdString = cmdString.replace("[", "");
+			cmdString = cmdString.replace("]", "");
+			
+			Process process = Runtime.getRuntime().exec(cmdString);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader stdErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			while((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+			while((s = stdErr.readLine()) != null) {
+				System.out.println(s);
+			}
+		}catch (Exception e) {
+			System.out.println("Error:");
+			e.printStackTrace();
+		}
 	}
 }
